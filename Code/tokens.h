500eced6c3c90effe74bbc7f87b67e792a300997
TWT <TWT2333@outlook.com>
2020-10-11 19:49:35 +0800

    doc(tokens.h): add type number assignment rules

--- Code/tokens.h
+++ Code/tokens.h
@@ -1,5 +1,5 @@
 /**
- * lexical token types naming rule:
+ * lexical token type naming rules:
  * 
  *      [`group`_[`category`_]]<name>
  * 
@@ -25,3 +25,31 @@
  * 
  */
 
+/**
+ * lexical token type number assignment rules:
+ * 
+ * typeno is a 32-bit unsigned int, which be divided into parts here:
+ * 
+ *          0xFF|FF|FF|FF
+ *            --|--|--|--
+ *            P1|P2|P3|P4
+ * 
+ * P1: the code of group:
+ *      - DATA  : 01
+ *      - KWD   : 02
+ *      - OP    : 03
+ * 
+ * P2: the code of category (optional):
+ *      there are categories under OP now,
+ *      for OP :
+ *          - REL   : 01
+ *          - CAL   : 02
+ *          - BOO   : 03
+ *     set this field to zero for no-cate token types.
+ * 
+ * P3 & P4: the code of name, P3 is necessary while
+ *          P4 is enabled when this type of token is a ASCII char
+ *          e.g. ">" "!" "=" "+"
+ *          when P4 is enabled, set P3 == P4, otherwise P4 = 0
+ *
+ */
