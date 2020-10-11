da0cda1e7571d15601255b98ea4c053f9ffff238
TWT <TWT2333@outlook.com>
2020-10-11 20:29:10 +0800

    feat: add definition for tokens

--- Code/tokens.h
+++ Code/tokens.h
@@ -50,6 +50,61 @@
  * P3 & P4: the code of name, P3 is necessary while
  *          P4 is enabled when this type of token is a ASCII char
  *          e.g. ">" "!" "=" "+"
- *          when P4 is enabled, set P3 == P4, otherwise P4 = 0
  *
  */
+
+#define G_DATA 01
+#define G_OP 02
+#define G_KWD 03
+
+#define C_OP_REL 01
+#define C_OP_BOO 02
+#define C_OP_CAL 03
+
+#define GEN(g, c, n, p) (0x##g##c##n##00 + p)
+
+#define DATA_INT GEN(G_DATA, 00, 01, 00)
+#define DATA_FLOAT GEN(G_DATA, 00, 02, 00)
+
+//***********************************************
+
+#define OP_REL_GE GEN(G_OP, C_OP_REL, 01, 00)
+#define OP_REL_LE GEN(G_OP, C_OP_REL, 02, 00)
+#define OP_REL_G GEN(G_OP, C_OP_REL, 03, '>')
+#define OP_REL_L GEN(G_OP, C_OP_REL, 04, '<')
+#define OP_REL_EQ GEN(G_OP, C_OP_REL, 05, 00)
+#define OP_REL_NE GEN(G_OP, C_OP_REL, 06, 00)
+//-----------------------------------------------
+#define OP_BOO_AND GEN(G_OP, C_OP_BOO, 01, 00)
+#define OP_BOO_OR GEN(G_OP, C_OP_BOO, 02, 00)
+#define OP_BOO_NOT GEN(G_OP, C_OP_BOO, 03, '!')
+//-----------------------------------------------
+#define OP_CAL_PLUS GEN(G_OP, C_OP_CAL, 01, '+')
+#define OP_CAL_MINUS GEN(G_OP, C_OP_CAL, 02, '-')
+#define OP_CAL_STAR GEN(G_OP, C_OP_CAL, 03, '*')
+#define OP_CAL_DIV GEN(G_OP, C_OP_CAL, 04, '/')
+//-----------------------------------------------
+#define OP_ASSIGN GEN(G_OP, 00, 01, '=')
+#define OP_DOT GEN(G_OP, 00, 02, '.')
+
+//***********************************************
+
+#define KWD_INT GEN(G_KWD, 00, 01, 00)
+#define KWD_FLOAT GEN(G_KWD, 00, 02, 00)
+#define KWD_STRUCT GEN(G_KWD, 00, 03, 00)
+#define KWD_RETURN GEN(G_KWD, 00, 04, 00)
+#define KWD_IF GEN(G_KWD, 00, 05, 00)
+#define KWD_ELSE GEN(G_KWD, 00, 06, 00)
+#define KWD_WHILE GEN(G_KWD, 00, 07, 00)
+
+//***********************************************
+
+#define ID GEN(00, 00, 01, 00)
+#define SEMI GEN(00, 00, 01, ';')
+#define COMMA GEN(00, 00, 01, ',')
+#define LP GEN(00, 00, 01, '(')
+#define RP GEN(00, 00, 01, ')')
+#define LB GEN(00, 00, 01, '[')
+#define RB GEN(00, 00, 01, ']')
+#define LC GEN(00, 00, 01, '{')
+#define RC GEN(00, 00, 01, '}')
