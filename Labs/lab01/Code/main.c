64b60ee4f6e9fb96c6e0f186289c896beac57890
TWT233 <TWT2333@outlook.com>
2020-10-14 03:56:19 -0700

    feat: add printer for main

--- Labs/lab01/Code/main.c
+++ Labs/lab01/Code/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+#define __MINUSES_GTREE_H_IMPLEMENTS__
+#include "gtree.h"
+
 extern int yyrestart(FILE*);
 extern int yyparse(void);
+extern gtree* root;
+
+size_t counter = 0;
+
+gtree* print_gtree(gtree* t) {
+  if (t == NULL) return NULL;
+  ++counter;
+
+  for (size_t i = 1; i < counter; i++) printf("  ");
+  printf("%s (%d)\n", (t->d->type == 0) ? t->d->name : t->d->val,
+         t->d->pos->first_line);
+  foreach_child(t, print_gtree);
+
+  --counter;
+  return t;
+}
 
 int main(int argc, char** argv) {
   if (argc <= 1) {
@@ -17,6 +36,10 @@ int main(int argc, char** argv) {
 
   yyrestart(f);
   yyparse();
+
+  print_gtree(root);
+
+  free_gtree(root);
 
   return 0;
 }
