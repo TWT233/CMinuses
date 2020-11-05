fc961f274f555bbd8f691048b53c9b692471a249
TWT233 <TWT2333@outlook.com>
2020-11-04 23:20:31 -0800

    refactor: use t_print() rather print_gtree()

--- Labs/lab01/Code/main.c
+++ Labs/lab01/Code/main.c
@@ -1,48 +1,12 @@
 #include <stdio.h>
 
-#include "syntax.tab.h"
-#define __MINUSES_GTREE_H_IMPLEMENTS__
 #include "gtree.h"
+#include "syntax.tab.h"
 
 extern int yyrestart(FILE*);
 extern int yyparse(void);
 extern int error_mark;
 extern gtree* root;
-
-size_t counter = 0;
-
-gtree* print_gtree(gtree* t) {
-  if (t == NULL || t->d->tn == -1) return NULL;
-  ++counter;
-
-  for (size_t i = 1; i < counter; i++) printf("  ");
-
-  switch (t->d->tn) {
-    case -1:
-      break;
-    case 0:
-      printf("%s (%d)\n", t->d->ts, t->d->pos->first_line);
-      break;
-    case ID:
-    case TYPE:
-      printf("%s: %s\n", t->d->ts, t->d->val_str);
-      break;
-    case INT:
-      printf("%s: %d\n", t->d->ts, t->d->val_int);
-      break;
-    case FLOAT:
-      printf("%s: %lf\n", t->d->ts, t->d->val_flt);
-      break;
-    default:
-      printf("%s\n", t->d->ts);
-      break;
-  }
-
-  t_c_foreach(t, print_gtree);
-
-  --counter;
-  return t;
-}
 
 int main(int argc, char** argv) {
   if (argc <= 1) {
@@ -59,7 +23,7 @@ int main(int argc, char** argv) {
   yyrestart(f);
   yyparse();
 
-  if (!error_mark) print_gtree(root);
+  if (!error_mark) t_print(root);
 
   t_free(root);
 
