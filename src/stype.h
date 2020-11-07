e83c77fa3f0f356af31a8cdc73dcf24f5d9da0f5
TWT233 <TWT2333@outlook.com>
2020-11-07 04:00:06 -0800

    feat: type defs for stype

--- src/stype.h
+++ src/stype.h
@@ -1,5 +1,34 @@
 #ifndef __MINUSES_STYPE_H__
 #define __MINUSES_STYPE_H__
 
+// ===============  Macros  ===============
 
+// ===============  Types  ===============
+
+// symbol type
+typedef struct stype stype;
+// field list
+typedef struct field field;
+
+struct stype {
+  enum { T_BASIC, T_ARRAY, T_STRUC } kind;
+  union {
+    enum { B_INT, B_FLOAT } basic;
+    struct {
+      stype* elem;
+      int size;
+    } array;
+    field* struc;
+  };
+};
+
+struct field {
+  char* name;
+  stype* type;
+  field* next;
+};
+
+// ===============  Func Decs  ===============
+
+// ===============  Macro Undef  ===============
 #endif  // __MINUSES_STYPE_H__
