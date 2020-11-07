86900878fe2b4338632d6a9989047c17acf534e3
TWT233 <TWT2333@outlook.com>
2020-11-07 04:01:26 -0800

    feat: type defs for symtable

--- src/symtable.h
+++ src/symtable.h
@@ -1,4 +1,36 @@
 #ifndef __MINUSES_HTABLE_H__
 #define __MINUSES_HTABLE_H__
 
+#include "stype.h"
+
+// ===============  Macros  ===============
+
+// ===============  Types  ===============
+
+typedef struct sym_list sym_list;
+typedef struct sym sym;
+typedef struct sym_table sym_table;
+
+// linked symbol entries
+struct sym_list {
+  sym* ptr;
+  sym_list* next;
+};
+
+// symbol
+struct sym {
+  char* name;
+  stype* type;
+};
+
+// symbol table
+struct sym_table {
+  sym_list** table;
+  size_t len;
+};
+
+// ===============  Func Decs  ===============
+
+// ===============  Macro Undef  ===============
+
 #endif  //__MINUSES_HTABLE_H__
