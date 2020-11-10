f0da5fe428b96110b81fde02db104202b457daab
TWT233 <TWT2333@outlook.com>
2020-11-09 21:00:56 -0800

    feat: basic init and getter

--- src/traverser.c
+++ src/traverser.c
@@ -1,1 +1,24 @@
 #include "traverser.h"
+
+#include <stdio.h>
+
+// ===============  Macros  ===============
+
+
+// ===============  Var Defs  ===============
+
+static sym_table* TABLE = NULL;
+
+// ===============  Func Defs  ===============
+
+void init_table() { TABLE = st_new(); }
+
+sym_table* get_table() {
+  if (TABLE == NULL) init_table();
+  return TABLE;
+}
+
+
+
+// ===============  Macro Undef  ===============
+
--- src/traverser.h
+++ src/traverser.h
@@ -1,5 +1,20 @@
 #ifndef __MINUSES_TRAVERSER_H__
 #define __MINUSES_TRAVERSER_H__
 
+#include "gtree.h"
+#include "symtable.h"
+
+// ===============  Macros  ===============
+
+// ===============  Types  ===============
+
+// ===============  Func Decs  ===============
+
+void init_table();
+
+sym_table* get_table();
+
+
+// ===============  Macro Undef  ===============
 
 #endif
